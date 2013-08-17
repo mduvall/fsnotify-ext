ba8d59727e3f14a40d26405b607be6f83a40543c
Matt DuVall <mduvall89@gmail.com>
2013-08-17 12:21:14 -0700

    WatchAll for entire directory

--- fsnotify-ext.go
+++ fsnotify-ext.go
@@ -1,11 +1,26 @@
 package fsnotify2
 
 import (
+	"errors"
 	"github.com/howeyc/fsnotify"
+	"os"
+	"path/filepath"
 )
 
 type Watcher struct {
 	fsnotify.Watcher
+}
+
+func (w *Watcher) WatchAll(path string) error {
+	watchFunc := func(path string, fi os.FileInfo, err error) error {
+		return w.Watch(path)
+	}
+
+	if err := filepath.Walk(path, watchFunc); err != nil {
+		return errors.New("Path given is not valid.")
+	}
+
+	return nil
 }
 
 // Return a new watcher of the extended type
